c27636a8d0bd3de98327a4a16713991e37e93d38
Kam Lotfull <kamlotfull@gmail.com>
2017-09-22 14:19:19 +0300

    logFunctionNameAdded

--- TinkoffChat/AppDelegate.swift
+++ TinkoffChat/AppDelegate.swift
@@ -14,8 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func logFunctionName(string: String = #function) {
+        print(string)
+        
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        logFunctionName()
         /*
         window = UIWindow.init(frame: UIScreen.main.bounds) // Задаем виндоу размером с экран
         if let keyWindow = window {
@@ -23,15 +28,33 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
             keyWindow.makeKeyAndVisible() // making our self created window visible
         }
         */
+        
         return true
     }
     
     func applicationWillResignActive(_ application: UIApplication) { // приложение при интеракции переходит в этот режим(при звонке)
-        
+        logFunctionName()
     }
     
     // аппдлегат также помогает слушать notifications
     // основные элементы uiview и uiviewC, от них все наследуются
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        logFunctionName()
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        logFunctionName()
+    }
+    
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        logFunctionName()
+    }
+    
+    func applicationWillTerminate(_ application: UIApplication) {
+        logFunctionName()
+        self.saveContext()
+    }
     
     // MARK: - Core Data stack
 
