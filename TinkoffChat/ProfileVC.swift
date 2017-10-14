8eed50afcf0a47e843ba67d6d76b25c6aec2044b
Kam Lotfull <kamlotfull@gmail.com>
2017-10-14 11:51:35 +0300

    not ready version. waiting for final task.

--- TinkoffChat/ProfileVC.swift
+++ TinkoffChat/ProfileVC.swift
@@ -8,38 +8,36 @@
 
 import UIKit
 
+
+
+struct Profile {
+    var name: String
+    var info: String
+    var image: UIImage?
+    
+    init(name: String, info: String, image: UIImage? = nil) {
+        self.name = name
+        self.info = info
+        self.image = image
+    }
+}
+
 class ProfileVC: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
     
-
     @IBAction func dismissButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
-    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
-        if editProfileButton != nil {
-            print("***", editProfileButton.frame, #function)
-        } else {
-            print("Couldn't print editProfileButton.frame because it is forceUnwrapped but nil at start of init(). It gets reference to UIButton object in loadView.")
-        }
-        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        activityIndicatorInit()
         
         logFunctionName()
         
         updateUI()
-        print("*** frame: ", editProfileButton.frame, "***") // frame here is scaled for iphone SE(or what device in storyboard)
     }
-    override func viewDidAppear(_ animated: Bool) {
-        print("*** frame: ", editProfileButton.frame, "***") // frame here is scaled for iphone 8(or what device in simulator)
-    }
+    
     override func viewWillAppear(_ animated: Bool) {
         let cornerRadius = (changePhotoButton.frame.height) / 2 + 8
         changePhotoButton.layer.cornerRadius = cornerRadius
@@ -49,20 +47,22 @@ class ProfileVC: UIViewController, UINavigationControllerDelegate, UIImagePicker
         // encoding
         
         //UserDefaults.standard.set(false, forKey: isProfileImageLoaded)
-        
+        // TODO: Get_Data_from_file
         if !UserDefaults.standard.bool(forKey: isProfileImageLoaded) {
             let image = UIImage(named: "GaroldWithPain.png")
             let imageData: NSData = UIImageJPEGRepresentation(image!, 1.0)! as NSData
             UserDefaults.standard.set(imageData, forKey: profileImageKey)
         }
+        // if !UserDefaults.standard.bool(forKey: )
         // Decode
         let data = UserDefaults.standard.object(forKey: profileImageKey) as! NSData
         let image = UIImage(data: data as Data)
         photoImageView.image = image
-        
         photoImageView.clipsToBounds = true
-        editProfileButton.layer.borderWidth = 1.0
-        editProfileButton.layer.cornerRadius = 12.0
+        editProfileWithGCDButton.layer.borderWidth = 1.0
+        editProfileWithGCDButton.layer.cornerRadius = 12.0
+        editProfileWithOperationButton.layer.borderWidth = 1.0
+        editProfileWithOperationButton.layer.cornerRadius = 12.0
     }
     func logFunctionName(method: String = #function) {
         print("Completed ProfileVC.\(lastMethod)\nStarted ProfileVC.\(method)")
@@ -99,31 +99,69 @@ class ProfileVC: UIViewController, UINavigationControllerDelegate, UIImagePicker
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
             photoImageView.image = image
-            let imageData: NSData = UIImageJPEGRepresentation(image, 1.0)! as NSData//UIImageJPEGRepresentation(image)! as NSData
-            UserDefaults.standard.set(imageData, forKey: profileImageKey)
-            UserDefaults.standard.set(true, forKey: isProfileImageLoaded)
-        } else {
-            // Error message here
+            
+            //let imageData: NSData = UIImageJPEGRepresentation(image, 1.0)! as NSData
+//            UserDefaults.standard.set(imageData, forKey: profileImageKey)
+//            UserDefaults.standard.set(true, forKey: isProfileImageLoaded)
         }
         picker.dismiss(animated: true, completion: nil)
     }
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
-    let isProfileImageLoaded = "isProfileImageLoaded"
-    let profileImageKey = "profileImage"
-    var lastMethod: String = "Opening VC"
+    
+    func activityIndicatorInit() {
+        activityIndicator.center = self.view.center
+        activityIndicator.hidesWhenStopped = true
+        activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyle.whiteLarge
+        view.addSubview(activityIndicator)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+    
+    
+    
+    
+    // MARK: @IB***
     @IBOutlet weak var changePhotoButton: UIButton!
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var definitionLabel: UILabel!
-    @IBOutlet weak var editProfileButton: UIButton!
-    @IBAction func editProfileButtonPressed(_ sender: Any) {
+    @IBOutlet weak var editProfileWithGCDButton: UIButton!
+    @IBOutlet weak var editProfileWithOperationButton: UIButton!
+    @IBAction func editProfileWithGCD(_ sender: Any) {
+        // TODO: save_data_GCD
+        activityIndicator.startAnimating()
+        let queue = DispatchQueue(label: "editProfileWithGCD.queue")
+        queue.async {
+            let image
+            if let data = UIImageJPEGRepresentation(image, 1.0) {
+                let filename = getDocumentsDirectory().appendingPathComponent("profile_photo.png")
+                try? data.write(to: filename)
+            }
+        }
+        
+    }
+    @IBAction func editProfileWithOperation(_ sender: Any) {
+        // TODO: save_data_GCD
+        activityIndicator.startAnimating()
+        save_data()
     }
     
-
-    
+    // MARK: Vars and Lets
+    let isProfileImageLoaded = "isProfileImageLoaded"
+    let isProfileNameLoaded = "isProfileNameLoaded"
+    let isProfileInfoLoaded = "isProfileInfoLoaded"
+    let profileNameKey = "profileName"
+    let profileInfoKey = "profileInfo"
+    let profileImageKey = "profileImage"
+    var lastMethod: String = "Opening VC"
+    var activityIndicator = UIActivityIndicatorView()
     
 }
 
